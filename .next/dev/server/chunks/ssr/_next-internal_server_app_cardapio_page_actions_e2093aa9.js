74c56e3a9eb41d30a68e1b8fcd282bcd1d6f3c1a
vk_delas <enzzobaraldo2008@gmail.com>
2026-01-27 17:04:57 -0300

    Remove .next do versionamento (build cache)

--- .next/dev/server/chunks/ssr/_next-internal_server_app_cardapio_page_actions_e2093aa9.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/cardapio/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_cardapio_page_actions_e2093aa9.js.map
